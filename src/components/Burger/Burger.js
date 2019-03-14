c4c6974bcd2ebb56206e8e2d711a6b4e40016c51
timzprof <timilehin.olumofin@gmail.com>
2019-03-14 09:19:43 +0100

    Covered 0 ingredients edge case

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
   .map(ingredientKey => {
     return [...Array(props.ingredients[ingredientKey])].map((_,i) => {
       return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
     });
-  });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start Adding Ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>

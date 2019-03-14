18cd6e52154cc62e6169bf846794fc4e07f9a05f
timzprof <timilehin.olumofin@gmail.com>
2019-03-14 08:49:49 +0100

    Implemented Static burger ingredients

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import classes from './App.module.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className={classes.App}>
+      <div>
         <Layout>
           <BurgerBuilder />
         </Layout>

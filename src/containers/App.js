138fddf51aeaab45a625b8ccde6255bdeaed7ba3
timzprof <timilehin.olumofin@gmail.com>
2019-03-14 07:51:20 +0100

    Added Layout Structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
+import Layout from '../components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className={classes.App}>
+        <Layout>
+          <p>Welcome</p>
+        </Layout>
       </div>
     );
   }

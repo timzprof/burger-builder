1464bc7b6af50774c21e48bca8d43e4ec13f6886
timzprof <timilehin.olumofin@gmail.com>
2019-03-14 07:57:35 +0100

    Restructured Project and added little custom styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
   <Aux>
     <div>
       Toolbar, Sidebar, Backdrop
     </div>
-    <main>
+    <main className={classes.Content}>
       { props.children }
     </main>
   </Aux>

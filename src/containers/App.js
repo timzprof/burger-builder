9e9040ae77891c0cce097097df01017587b799ce
timzprof <timilehin.olumofin@gmail.com>
2019-03-14 07:35:26 +0100

    Added setup files

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+      </div>
+    );
+  }
+}
+
+export default App;

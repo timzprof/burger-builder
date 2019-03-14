f30751a39304840662f071d890bb49680d27fff2
timzprof <timilehin.olumofin@gmail.com>
2019-03-14 16:47:42 +0100

    Customized Modal buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,6 +72,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   }
 
+  purchaseContinueHandler = () => {
+    alert('Order Made');
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -82,7 +86,11 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary 
+            ingredients={this.state.ingredients}
+            purchaseCanceled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}   
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls 

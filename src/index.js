02abb8dfe9a8b180e4347f9cc03c334f52d899c1
timzprof <timilehin.olumofin@gmail.com>
2019-05-11 02:14:03 +0100

    Secured Redux store for Development only

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
-import orderReducer from './store/reducers/order';
-import authReducer from './store/reducers/auth';
+import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import thunk from "redux-thunk";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+	process.env.NODE_ENV === "development"
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null || compose;
 
 const rootReducer = combineReducers({
 	burgerBuilder: burgerBuilderReducer,
